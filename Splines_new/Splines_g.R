b09736a202abe4af2edb666cb1f6dfc1ffeb61f4
yansongruc <65059229+yansongruc@users.noreply.github.com>
2020-06-26 06:17:21 +0800

    Update Splines_g.R

--- Splines_new/Splines_g.R
+++ Splines_new/Splines_g.R
@@ -1,5 +1,7 @@
 library(mvtnorm)
 library(MASS)
+library(MaxPro)
+library(nabor)
 library(randtoolbox)
 library(gss)
 library(foreach)
@@ -7,37 +9,24 @@ library(doParallel)
 library(ggplot2)
 #source("ssa.R")
 #source("esps-m.R")
-<<<<<<< HEAD
-=======
 #source("ABS_select.R")
->>>>>>> f7fcb26... Splines
 #source("MED_select.R")
 
 ################################################################################
 # Setting 1: function g in Meng's first setting                                #
 #            Sobol sequence locations                                          #
-<<<<<<< HEAD
-#            Full sample sizes are c(2^10, 2^11, 2^12, 2^13, 2^14)             #
-=======
 #            Full sample sizes are c(2^12, 2^13, 2^14, 2^15, 2^16)             #
->>>>>>> f7fcb26... Splines
 #            Subsample sizes are k1=10*N^(1/9) and k2=5*N^(2/9)                #
 ################################################################################
 set.seed(100)
 nloop=100
-<<<<<<< HEAD
-NN=2^(seq(10,14,1))
-ntest=5000
-SNR=2                         #signal noise ratio 5 or 2
-=======
 NN=2^(seq(12,16,1))
 ntest=5000
 SNR=5                         #signal noise ratio 5 or 2
->>>>>>> f7fcb26... Splines
 sub1_meta=round(10*NN^(1/9))
-#sub1_meta=c(22,23,25,27,30)
+#sub1_meta=25 27 29 32 34
 sub2_meta=round(5*NN^(2/9))
-#sub2_meta=c(23,27,32,37,43)
+#sub2_meta=32 37 43 50 59
 
 # function g
 pp=2
@@ -51,14 +40,6 @@ gene_y = function(x){
 }
 
 # Generate testing data: sobol; on [0,1]^2
-<<<<<<< HEAD
-X_test<-as.data.frame(sobol(n=ntest ,dim = pp, init = T, scrambling = 1, seed=100))
-y.t<-gene_y(X_test)
-
-# using a part of data to find suitable p, lambda, theta by esps-m.R
-sam.size <- ceiling(50*(NN[5]^{1/4}))
-X<-as.data.frame(sobol(n=NN[5] ,dim = pp, init = T, scrambling = 1, seed=100))
-=======
 X_test<-as.data.frame(sobol(n=ntest ,dim = pp))
 y.t<-gene_y(X_test)
 
@@ -66,7 +47,6 @@ y.t<-gene_y(X_test)
 set.seed(100)
 sam.size <- ceiling(50*(NN[5]^{1/4}))
 X<-as.data.frame(sobol(n=NN[5] ,dim = pp))
->>>>>>> f7fcb26... Splines
 y<-gene_y(X)
 esps.re <- esps(y~.+.^2, data=data.frame(y, X), sam.size,r=3)
 
@@ -83,11 +63,7 @@ for(jj in 1:length(NN)){
   
   #Design matrix
   set.seed(100)
-<<<<<<< HEAD
-  X<-as.data.frame(sobol(n=N ,dim = pp, init = T, scrambling = 1, seed=100))
-=======
   X<-as.data.frame(sobol(n=N ,dim = pp))
->>>>>>> f7fcb26... Splines
   y.true<-gene_y(X)
   
   ###################################################################
@@ -181,23 +157,6 @@ for(jj in 1:length(NN)){
 
 }
 
-<<<<<<< HEAD
-UNIF12=log(UNIF1_meta)
-UNIF22=log(UNIF2_meta)
-ABS12=log(ABS1_meta)
-ABS22=log(ABS2_meta)
-SBS12=log(SBS1_meta)
-SBS22=log(SBS2_meta)
-MED12=log(MED1_meta)
-MED22=log(MED2_meta)
-
-mse.mat2<-data.frame(mse=c(apply(UNIF12,2,mean), apply(UNIF22,2,mean), apply(ABS12,2,mean), 
-                          apply(ABS22,2,mean), apply(SBS12,2,mean), apply(SBS22,2,mean),
-                          apply(MED12,2,mean),apply(MED22,2,mean)),
-                    sd=c(apply(UNIF12,2,sd), apply(UNIF22,2,sd), apply(ABS12,2,sd), 
-                         apply(ABS22,2,sd), apply(SBS12,2,sd), apply(SBS22,2,sd),
-                         apply(MED12,2,sd),apply(MED22,2,sd)),
-=======
 UNIF1=log(UNIF1_meta)
 UNIF2=log(UNIF2_meta)
 ABS1=log(ABS1_meta)
@@ -213,16 +172,11 @@ mse.mat<-data.frame(mse=c(apply(UNIF1,2,mean), apply(UNIF2,2,mean), apply(ABS1,2
                     sd=c(apply(UNIF1,2,sd), apply(UNIF2,2,sd), apply(ABS1,2,sd), 
                          apply(ABS2,2,sd), apply(SBS1,2,sd), apply(SBS2,2,sd),
                          apply(MED1,2,sd),apply(MED2,2,sd)),
->>>>>>> f7fcb26... Splines
                     Method=factor(rep(c("UNIF1","UNIF2","ABS1","ABS2","SBS1","SBS2","GBMED1","GBMED2"), each=length(NN))),
                     n=rep(log(NN),8))
 
 pd <- position_dodge(0.2)
-<<<<<<< HEAD
-p1 = ggplot(mse.mat2,aes(x=n,y=mse,group=Method,colour=Method))
-=======
 p1 = ggplot(mse.mat,aes(x=n,y=mse,group=Method,colour=Method))
->>>>>>> f7fcb26... Splines
 p23 = p1+theme_light()+theme(panel.grid.major = element_blank(),
                           panel.grid.minor = element_blank(),
                           panel.border = element_rect(colour = "black"),
